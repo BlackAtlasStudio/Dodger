9a41e55e528d5be0168079a27868576d81f4cebe
Connor Douglas <cjdouglas96@gmail.com>
2017-11-14 19:27:56 -0700

    Add simple object pooling script

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public List<GameObject> instances = new List<GameObject>();
+    public GameObject prefab;
+    public string ID;
+    public int maxEntities;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="T:ObjectPool"/> class with string ID, GameObject prefab and a max entities integer.
+    /// </summary>
+    /// <param name="id">Identifier.</param>
+    /// <param name="p">P.</param>
+    /// <param name="max">Max.</param>
+    public ObjectPool (string id, GameObject p, int max)
+    {
+        ID = id;
+        prefab = p;
+        maxEntities = max;
+    }
+
+    private void Start()
+    {
+        instances = new List<GameObject>();
+
+        //TODO Spawn a set number of starting instances for the pool to draw from
+    }
+
+    private void OnDestroy()
+    {
+        //TODO Destroy all entities in the pool
+    }
+}
---
+++ Assets/Scripts/ObjectPoolManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolManager : MonoBehaviour
+{
+    public Dictionary<string, ObjectPool> pools;
+    public int defaultMaxEntities;
+
+    /// <summary>
+    /// Creates a new ObjectPool with an identifier and prefab.
+    /// </summary>
+    /// <param name="ID">Identifier.</param>
+    /// <param name="prefab">Prefab.</param>
+    public ObjectPool AddPool(string ID, GameObject prefab)
+    {
+        return AddPool(ID, prefab, defaultMaxEntities);
+    }
+
+    /// <summary>
+    /// Creates a new ObjectPool with an identifier and prefab with a max number of entities in the pool.
+    /// </summary>
+    /// <param name="ID">Identifier.</param>
+    /// <param name="prefab">Prefab.</param>
+    /// <param name="maxEntity">Max entity count.</param>
+    public ObjectPool AddPool(string ID, GameObject prefab, int maxEntity)
+    {
+        ObjectPool pool = new ObjectPool(ID, prefab, maxEntity);
+        pools.Add(ID, pool);
+        return pool;
+    }
+
+    /// <summary>
+    /// Gets a pool from a string identifier.
+    /// </summary>
+    /// <returns>The pool.</returns>
+    /// <param name="ID">Identifier.</param>
+    public ObjectPool GetPool(string ID)
+    {
+        return pools[ID];
+    }
+
+    /// <summary>
+    /// Gets a pool from its pooled prefab.
+    /// </summary>
+    /// <returns>The pool.</returns>
+    /// <param name="prefab">Prefab.</param>
+    public ObjectPool GetPool(GameObject prefab)
+    {
+        foreach (ObjectPool p in pools.Values) {
+            if (p.prefab = prefab)
+                return p;
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// Removes a pool with ID.
+    /// </summary>
+    /// <param name="ID">Identifier.</param>
+    public void RemovePool(string ID)
+    {
+        pools.Remove(ID);
+    }
+
+    /// <summary>
+    /// Removes a pool spawning a certain prefab/
+    /// </summary>
+    /// <param name="prefab">Prefab.</param>
+    public void RemovePool(GameObject prefab)
+    {
+        ObjectPool pool = GetPool(prefab);
+        if (pool != null)
+            pools.Remove(pool.ID);
+    }
+
+    /// <summary>
+    /// Removes a pool.
+    /// </summary>
+    /// <param name="pool">Pool.</param>
+    public void RemovePool(ObjectPool pool)
+    {
+        pools.Remove(pool.ID);
+    }
+}
