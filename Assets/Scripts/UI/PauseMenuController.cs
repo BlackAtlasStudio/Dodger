7ec8c4f6c9322c431350875017ac89356a064f97
Connor Douglas <cjdouglas96@gmail.com>
2017-11-18 15:01:05 -0700

    More additions

---
+++ Assets/Scripts/UI/PauseMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [Header("Menu Settings")]
+    public bool stopTimeOnPause;
+    public bool IsPaused { get; private set; }
+
+    private Canvas menuCanvas;
+
+    public void Pause()
+    {
+        IsPaused = true;
+        if (stopTimeOnPause) Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        IsPaused = false;
+        if (stopTimeOnPause) Time.timeScale = 1f;
+    }
+
+    private void Start()
+    {
+        menuCanvas = GetComponent(typeof(Canvas)) as Canvas;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused) Resume();
+            else Pause();
+        }
+    }
+}
